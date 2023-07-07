e22ca02979e9368570141647ecb259e32d8803c0
Yrjo47 <arhegor@gmail.com>
2023-07-07 22:01:49 +0300

    Initial commit

---
+++ front/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './pages/Home';
+import News from './pages/News';
+import Feedback from './pages/Feedback';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <News />
+      <Feedback />
+    </div>
+  );
+}
+
+export default App;
